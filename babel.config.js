4c016818465c9740d06d41292fc48104dcd05664
rrahulvishwakarma007-lgtm <rrahulvishwakarma007@gmail.com>
2026-04-18 14:24:34 +0000

    Fix babel config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [require('expo/node_modules/babel-preset-expo')],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: ['babel-preset-expo'],
   };
 };
