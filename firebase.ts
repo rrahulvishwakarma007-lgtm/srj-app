9ab664872d3484076920a16723bc3081c2eac173
rrahulvishwakarma007-lgtm <rrahulvishwakarma007@gmail.com>
2026-04-24 09:56:17 +0530

    Fix storageBucket line in firebase configuration

--- firebase.ts
+++ firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey:            'AIzaSyAKnJ2EmN1q8BHZfBc2GWFbDtyzGg7H3cI',
   authDomain:        'srj-app-f2ab4.firebaseapp.com',
   projectId:         'srj-app-f2ab4',
-storageBucket:       "srj-app-f2ab4.appspot.com"
+storageBucket:       "srj-app-f2ab4.appspot.com",
   messagingSenderId: '895107611382',
   appId:             '1:895107611382:web:0312e643918eac606e0ad7',
 };
