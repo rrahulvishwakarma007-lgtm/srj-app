598d61576ab6ddbcd6bb8a73c25f41e620beb7e7
rrahulvishwakarma007-lgtm <rrahulvishwakarma007@gmail.com>
2026-04-24 09:53:22 +0530

    Add files via upload

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey:            'AIzaSyAKnJ2EmN1q8BHZfBc2GWFbDtyzGg7H3cI',
+  authDomain:        'srj-app-f2ab4.firebaseapp.com',
+  projectId:         'srj-app-f2ab4',
+storageBucket:       "srj-app-f2ab4.appspot.com"
+  messagingSenderId: '895107611382',
+  appId:             '1:895107611382:web:0312e643918eac606e0ad7',
+};
+
+// Prevent duplicate app init on hot reload
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+export const db      = getFirestore(app);
+export const auth = getAuth(app);
+export default app;
